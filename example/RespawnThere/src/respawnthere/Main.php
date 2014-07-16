b9838dc9a6b139b8b8ac24194c83fe9b791aa8fc
PEMapModder <pemapmodder1970@gmail.com>
2014-07-16 21:25:34 +0800

    RespawnThere: Register event

--- example/RespawnThere/src/respawnthere/Main.php
+++ example/RespawnThere/src/respawnthere/Main.php
@@ -2,7 +2,13 @@
 
 namespace respawnthere;
 
+use pocketmine\event\player\PlayerRespawnEvent;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase{
+class Main extends PluginBase
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	public function onRespawn(PlayerRespawnEvent $event){
+	}
 }
