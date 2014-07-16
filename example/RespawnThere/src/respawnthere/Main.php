709a32b728c01780b2d51526495acf025883a7ca
PEMapModder <pemapmodder1970@gmail.com>
2014-07-16 21:43:45 +0800

    Check if $levelObject is a valid world, then commit suicide (self-disable) on failure to load the world

--- example/RespawnThere/src/respawnthere/Main.php
+++ example/RespawnThere/src/respawnthere/Main.php
@@ -3,6 +3,7 @@
 namespace respawnthere;
 
 use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\level\Level;
 use pocketmine\level\Position;
 use pocketmine\plugin\PluginBase;
 
@@ -18,6 +19,17 @@ class Main extends PluginBase{
 		$z = $position["z"];
 		$level = $position["level"];
 		$levelObject = $this->getServer()->getLevelByName($level);
+		if(!($levelObject instanceof Level)){
+			// level $level is not loaded. Let's try loading it ourselves :)
+			$this->getServer()->loadLevel($level); // load a level called $level
+			$levelObject = $this->getServer()->getLevelByName($level); // try to get the object again
+			if(!($levelObject instanceof Level)){
+				// uh-oh, the level is still not loaded
+				$this->getLogger()->critical("Unable to load level \"$level\"!"); // send console a message that the level cannot be loaded
+				$this->setEnabled(false); // commit suicide! Disable yoruself!
+				return;
+			}
+		}
 		$this->respawnPosition = new Position($x, $y, $z, $levelObject);
 	}
 	public function onRespawn(PlayerRespawnEvent $event){
