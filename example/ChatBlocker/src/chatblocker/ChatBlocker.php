5eeb621551aa133d4af431576000bee5fd0b0f3e
PEMapModder <pemapmodder1970@gmail.com>
2014-12-20 14:33:59 +0800

    Added an event handler with method stubs

--- example/ChatBlocker/src/chatblocker/ChatBlocker.php
+++ example/ChatBlocker/src/chatblocker/ChatBlocker.php
@@ -3,6 +3,7 @@
 namespace chatblocker;
 
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\plugin\PluginBase;
 
 class ChatBlocker extends PluginBase implements Listener{
@@ -10,5 +11,21 @@ class ChatBlocker extends PluginBase implements Listener{
 		$this->saveDefaultConfig(); // saves config.yml if it doesn't already exist
 		$this->reloadConfig(); // pre-load the config so that the file is loaded in startup time rather than runtime, improving server performance
 		$this->getPluginManager()->registerEvents($this, $this);
+	}
+	public function onChat(PlayerChatEvent $evt){
+		$player = $evt->getPlayer();
+		if($player->hasPermission("chatblocker.bypass")){
+			return;
+		}
+		$message = $evt->getMessage();
+		if($this->matches($message)){
+			$this->executeOn($evt);
+		}
+	}
+	public function matches($message){	
+		// TODO
+	}
+	public function executeOn(PlayerChatEvent $e){
+		// TODO
 	}
 }
