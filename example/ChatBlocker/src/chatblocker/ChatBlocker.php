da106012174d9e693807a8c446fa6c5bc7282233
PEMapModder <pemapmodder1970@gmail.com>
2014-12-20 14:09:41 +0800

    Save and preload the config in onEnable(); register event listener

--- example/ChatBlocker/src/chatblocker/ChatBlocker.php
+++ example/ChatBlocker/src/chatblocker/ChatBlocker.php
@@ -2,7 +2,13 @@
 
 namespace chatblocker;
 
+use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 
-class ChatBlocker extends PluginBase{
+class ChatBlocker extends PluginBase implements Listener{
+	public function onEnable(){
+		$this->saveDefaultConfig(); // saves config.yml if it doesn't already exist
+		$this->reloadConfig(); // pre-load the config so that the file is loaded in startup time rather than runtime, improving server performance
+		$this->getPluginManager()->registerEvents($this, $this);
+	}
 }
