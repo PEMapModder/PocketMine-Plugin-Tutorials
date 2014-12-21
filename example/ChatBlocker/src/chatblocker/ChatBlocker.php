afb361e2d243b7cbf7aa4a0cd7bd7476ab4862c5
Falkirks <Falkirks@users.noreply.github.com>
2014-12-20 18:01:52 -0800

    Closes #7

--- example/ChatBlocker/src/chatblocker/ChatBlocker.php
+++ example/ChatBlocker/src/chatblocker/ChatBlocker.php
@@ -10,7 +10,7 @@ class ChatBlocker extends PluginBase implements Listener{
 	public function onEnable(){
 		$this->saveDefaultConfig(); // saves config.yml if it doesn't already exist
 		$this->reloadConfig(); // pre-load the config so that the file is loaded in startup time rather than runtime, improving server performance
-		$this->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 	public function onChat(PlayerChatEvent $evt){
 		$player = $evt->getPlayer();
